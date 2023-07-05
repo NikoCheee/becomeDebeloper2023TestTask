e7c54599f97c74adc9620769e57ee401486fdaaa
NikoCheee <nikita3ch@gmail.com>
2023-07-05 17:06:44 +0300

    Add main function for user input in a view

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from forms import UniqueCharacterForm
+from module import unique_character
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BecomeADeveloper2023'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = UniqueCharacterForm()
     if form.validate_on_submit():
         text = form.text.data
-    return render_template('index.html', form=form)
+        value = unique_character(text)
+    else:
+        value = None
+    return render_template('index.html', form=form, answer=value)
