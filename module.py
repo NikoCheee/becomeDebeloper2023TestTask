fd7416e480f5a4c2e20576716ee4fb9dff0a23a1
NikoCheee <nikita3ch@gmail.com>
2023-07-05 16:09:17 +0300

    Add main task function to module

---
+++ module.py
@@ -0,0 +1,20 @@
+def unique_character(text):
+    for char in text:
+        if char in '.@\'\`/,:$;-!#№%*&?)({}][|':
+            text = text.replace(char, '')
+
+    splitted = text.split()
+    done = []
+
+    for element in splitted:
+        for char in element:
+            count = element.count(char)
+            if count == 1:
+                done.append(char)
+                break
+
+    for element in done:
+        final = done.count(element)
+        if final == 1:
+            return element
+    return
